9b18f0c3794b23b6e46abf227eb24abe94d96ce2
2023125072 <doctor031699@naver.com>
2026-04-20 16:33:45 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(void) {
+	int n, max_num = 0, max_ch =0, cnt_num=0, cnt_char = 0;
+	char a;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n ; i++) {
+	
+		scanf(" %c", &a);
+		
+		if (a >= 'a' && a<='z') {
+			cnt_num=0;
+
+			cnt_char++;
+			
+		
+		}
+		if (a >= '1' && a < '9') {
+			cnt_char=0;
+
+			cnt_num++;
+			
+		}
+
+			if (cnt_char > max_ch) max_ch = cnt_char;
+			if (cnt_num > max_num) max_num = cnt_num;
+		}
+		
+		printf("%d\n", max_ch);
+		printf("%d\n", max_num);
+
+		
+		return 0;
+	}
