180a813253e716428f7d547d0c98436b6359e6a4
2023125072 <doctor031699@naver.com>
2026-04-20 16:47:35 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,24 +2,24 @@
 
 int main(void) {
 	int a, b, cnt=1;
-	scanf("%d", &a);
+	scanf("%d", &a);// 정답 입력
 
 	do {
-		scanf("%d", &b);
+		scanf("%d", &b);//예측 값 입력
 
-		if (a < b) {
+		if (a < b)/*정답보다 입력이 크면*/ {
 			printf("%d>?\n", b);
 		}
-		if (a > b) {
+		if (a > b)/*정답보다 입력이 작으면*/ {
 			printf("%d<?\n", b); 
 		}
-		if (a == b) {
+		if (a == b)/*정답과 입력이 같아서 맞췄으면*/ {
 			printf("%d==?\n", b);
-			printf("%d", cnt);
+			printf("%d", cnt);//시도 횟수 출력
 		}
-		cnt++;
+		cnt++;//시도 횟수 카운트 하기위해 하나씩 증가
 		
-	}while(a!=b);
+	}while(a!=b); //정답 값이랑 입력 값이랑 다른 경우 반복 같으면 반복 빠져나옴.
 
 
 	return 0;
